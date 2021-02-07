98eb02630b179b5bcd02d39530a643001499a789
Ivan Veselov <veselov@gmail.com>
2021-02-07 20:14:42 +0000

    Further PEP-8 stylistic changes.

--- utilities.py
+++ utilities.py
@@ -3,6 +3,7 @@ import statistics
 
 CARD_ORDER = "23456789TJQKA"
 
+
 def avg(vals):
     return safe_div(sum(vals), len(vals))
 
@@ -12,8 +13,10 @@ def safe_div(numer, denom) -> int:
         return 0
     return numer / denom
 
+
 def hand_ranks():
     return {row[1]: float(row[0]) for row in csv.reader(open("resources/hand_order.txt"))}
+
 
 def median(vals):
     if len(vals) == 0:
